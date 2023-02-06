2ae42ab06a37ef03debf115e0847bc5d415f2900
Pablo Zagni <pablozagniyahoo.com.ar>
2023-02-06 10:46:17 -0300

    test

---
+++ src/Providers/FlowbiteBladeComponentsProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FlowbiteBladeComponents;
+ 
+use Illuminate\Support\ServiceProvider;
+ 
+class FlowbiteBladeComponentsProvider extends ServiceProvider {
+
+    public function boot() {
+        
+    }
+
+    public function register()
+    {
+
+    }
+}
