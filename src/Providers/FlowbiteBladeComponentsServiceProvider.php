47f24f1a6a90a497ba48729622cf6a060386b222
Pablo Zagni <pablozagniyahoo.com.ar>
2023-02-06 15:33:18 -0300

    Fixes!

--- src/Providers/FlowbiteBladeComponentsServiceProvider.php
+++ src/Providers/FlowbiteBladeComponentsServiceProvider.php
@@ -1,20 +1,29 @@
 <?php
 
-namespace FlowbiteBladeComponents\Providers;
- 
+namespace Pablozagni\FlowbiteBladeComponents\Providers;
+
 use Illuminate\Support\ServiceProvider;
- 
-class FlowbiteBladeComponentsServiceProvider extends ServiceProvider {
+
+class FlowbiteBladeComponentsServiceProvider extends ServiceProvider
+{
 
     public function boot()
     {
+        // Loading the views.
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'flowbite-blade-components');
+
+        // Publishing the views.
         $this->publishes([
-            '/resources/views/components' => base_path('/resources/view/components'),
-        ],'flowbite-blade-components.views');
+            __DIR__ . '/../../resources/views' => resource_path('views/vendor/flowbite-blade-components'),
+        ], 'FlowbiteBladeComponents-views');
+
+//        $this->publishes([
+//            __DIR__.'/../../resources/views' => base_path('/resources/view/Components'),
+//        ], 'FlowbiteBladeComponents-views');
     }
 
     public function register()
     {
-
+        //
     }
 }
