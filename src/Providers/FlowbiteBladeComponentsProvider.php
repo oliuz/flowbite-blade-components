3e9804e19dca9333585eeb0c956516e60771af4a
Pablo Zagni <pablozagniyahoo.com.ar>
2023-02-06 12:21:27 -0300

    Commit

--- src/Providers/FlowbiteBladeComponentsProvider.php
+++ src/Providers/FlowbiteBladeComponentsProvider.php
@@ -4,10 +4,13 @@ namespace FlowbiteBladeComponents;
  
 use Illuminate\Support\ServiceProvider;
  
-class FlowbiteBladeComponentsProvider extends ServiceProvider {
+class FlowbiteBladeComponentsServiceProvider extends ServiceProvider {
 
-    public function boot() {
-        
+    public function boot()
+    {
+        $this->publishes([
+            '/resources/views/publishable' => base_path('/resources/view/Components'),
+        ],'FlowbiteBladeComponents-views');
     }
 
     public function register()
