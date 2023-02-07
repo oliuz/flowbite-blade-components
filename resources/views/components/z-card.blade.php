7b52ab75aba8f51e916d41ea495e880145f88227
Pablo Zagni <pablozagniyahoo.com.ar>
2023-02-06 23:09:31 -0300

    more compos

---
+++ resources/views/components/z-card.blade.php
@@ -0,0 +1,6 @@
+<div {{ $attributes->merge(['class'=>'bg-white shadow-md rounded px-8 pt-4 pb-2 mb-4']) }}>
+    <h1 class="text-lg font-bold mt-2 text-gray-800">{{ $title }}</h1>
+    <p class="text-gray-600 text-base mt-2">
+        {{ $body }}
+    </p>
+</div>
---
+++ resources/views/components/z-form.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge(['class' => 'w-full grid grid-cols-3']) }} >
+    {{ $slot }}    
+</div>
