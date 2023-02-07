52acf32961031d9940e2c9a6d699d9953c8e33c5
Pablo Zagni <pablozagniyahoo.com.ar>
2023-02-06 23:21:06 -0300

    input text fix

--- resources/views/components/inputs/z-text.blade.php
+++ resources/views/components/inputs/z-text.blade.php
@@ -5,7 +5,7 @@
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="name" name="name" type="text" value="{{ $value }}" autofocus>
+      <input class="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="{{ $name }}" name="{{ $name }}" type="text" value="{{ $value }}" autofocus>
       @error('name')
           <span class="text-sm text-red-400">{{ $message }}</span>
       @enderror
